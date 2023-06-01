c825be06444b21915a25022516a6ba367a6718dd
Amuga <43413033+Amuga@users.noreply.github.com>
2023-06-01 14:03:40 +0200

    Updated with some style changes/code cleanup
    Updated with some style changes/code cleanup and added basic support for clicking tiles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useState } from 'react';
+import { tabsConditions } from './gameConditions'
 import {Grid} from '@mui/material';
 import { TextField } from '@mui/material';
 import { Button } from '@mui/material';
@@ -8,14 +9,18 @@ import './App.css';
 function App() {
   const [playerName, setPlayerName] = useState<string>('Nameless Scum')
   const [bingoCards, setBingoCards] = useState<string[]>([])
-  const listOptions: string[] = ['ranger die to a sneeze (ranger die before day 6)','Ranger Muton Aggro','AI Derp','Sawmill Bop',
-    'Ballista didnt shoot','Mute 2 hit Crit','Soldier ded',
-    'Chicken Check','Mountain Donut','Worst Mayors','Micro fail','Tea Time','Muton one ballsita pull','Chokemap (one entrance)',
-    'Soldier on doomtown side start','5 volcano stack','Harpy ranger pull','Double taco trucks','3 gold one stack',
-    '3 zombies one Scratching post','stuck giant','No wood >4','Mayor joke','spinning muton','Koi pond!',
-    'over 100 gold mine', 'Great lake', 'Barracks before Woodshop', 'Condition Example 29', 'Condition Example 30']
-  const getRandomNumber = (maxValue: number): number => {
+  //TODO Implement check for maybe multiple games?
+  const conditionList = tabsConditions.slice()
+
+    const getRandomNumber = (maxValue: number): number => {
     return Math.floor(Math.random() * maxValue)
+  }
+
+  //TODO: Export to a separate bingo card component file probably
+
+  const handleCardClicked = (e: React.MouseEvent<HTMLDivElement>) => {
+    console.log('card clicked', e.target)
+    e.currentTarget.classList.toggle('card-clicked')
   }
 
   interface BingoCard {
@@ -23,21 +28,20 @@ function App() {
   }
 
   const Card: FC<BingoCard> = ({card}) => {
-    return <Grid xs={2.4} className='bingo-card'>{card}</Grid>
+    return <Grid item xs={2.4} onClick={handleCardClicked} className='bingo-card'>{card}</Grid>
 
   }
   const handleButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     console.log(`Generating for bing card for ${playerName}`)
     if (bingoCards.length >= 25 ) {
-      console.log('huh')
       setBingoCards([])
     }
     for (let i: number = 0; i < 25 ; i++) {
-      const randomItem: string = listOptions[getRandomNumber(listOptions.length)]
+      const randomItem: string = conditionList[getRandomNumber(conditionList.length)]
       setBingoCards(prevValues => [...prevValues ,randomItem])
-      listOptions.splice(listOptions.indexOf(randomItem), 1)
+      conditionList.splice(conditionList.indexOf(randomItem), 1)
     }
-    console.log('bingo card', bingoCards)
+    console.log('bingo cards', bingoCards)
 
   }
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -56,11 +60,11 @@ function App() {
               </div>
           </>
         ) : (<>
-          <p>Meaty's TABS Bingo
-          <div className='button'>{playerName}'s Bingo</div>
-          </p>
+          <p>Meaty's TABS Bingo</p>
+          <p>{playerName}'s Bingo</p>
+
             <Grid container spacing={2} className='bingo-board'>
-              {bingoCards.map(c => <Card card={c} key={`card ${c}`}/>)}
+              {bingoCards.map((c, index) => <Card card={c} key={index}/>)}
             </Grid>
             <div className="button">
               <Button variant="contained" onClick={handleButtonClick}>Re-generate</Button>
---
+++ src/gameConditions.tsx
@@ -0,0 +1,6 @@
+export const tabsConditions: string[] = ['ranger die to a sneeze (ranger die before day 6)','Ranger Muton Aggro','AI Derp','Sawmill Bop',
+'Ballista didnt shoot','Mute 2 hit Crit','Soldier ded',
+'Chicken Check','Mountain Donut','Worst Mayors','Micro fail','Tea Time','Muton one ballsita pull','Chokemap (one entrance)',
+'Soldier on doomtown side start','5 volcano stack','Harpy ranger pull','Double taco trucks','3 gold one stack',
+'3 zombies one Scratching post','stuck giant','No wood >4','Mayor joke','spinning muton','Koi pond!',
+'over 100 gold mine', 'Great lake', 'Barracks before Woodshop', 'Condition Example 29', 'Condition Example 30']
